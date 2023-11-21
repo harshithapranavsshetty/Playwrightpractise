3888e502e21f5b79aa958890dfca00a0542626bf
harshitha <hharshithahm@gmail.com>
2023-11-21 18:09:46 +0530

    harshitha <hharshithahm@gmail.com>

---
+++ playwrightpractisceproject/src/test/java/Pages/Login.java
@@ -0,0 +1,23 @@
+package Pages;
+
+import java.util.Collections;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class Login {
+	public static void main(String[] args) {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch(
+                    new BrowserType.LaunchOptions().setHeadless(false)
+                            .setArgs(Collections.singletonList("--start-maximized")));
+            BrowserContext context = browser.newContext(
+                    new Browser.NewContextOptions().setViewportSize(null));
+            Page page = context.newPage();
+            page.navigate(" http://aidas-dashboard.atparui.com:1080");
+}
+	}
+}
